4b221f56f1e82abbd65cd7730bfddbb1b457afde
Rosie Allison-Corio <rosievespa@gmail.com>
2025-01-23 11:29:34 +0000

    animated spinning cookie, implemented cps setInterval function

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # week-three-project
+https://awik.io/rotate-element-indefinitely-css-animation/#:~:text=You%20can%20rotate%20an%20element,give%20the%20element%20an%20ID.&text=Then%20set%20the%20animation%20property,taking%203%20seconds%20to%20complete.
+
+^^ 
+I used this code to make my cookie rotate.
+
+
+next commit: animated spinning cookie, implemented cps setInterval function.
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const clicker = document.getElementById('clicker')
 const cookieCount = document.getElementById('cookieCount')
 const shopDiv = document.getElementById('shopDiv')
-let cps = document.getElementById('perSec')
+const perSec = document.getElementById('perSec')
 const autoClicker = document.getElementById('displayAutoClicker')
 const buyClicker = document.getElementById('buyClicker')
 
@@ -18,19 +18,47 @@ function handleClick() {
     localStorage.setItem('count', currentCount)
 }
 
+//need to store value as number
+let cps = 1
+perSec.textContent = cps
+//add cps interval function
+cpsInterval = setInterval(() => {
+    currentCount = parseInt(currentCount) + cps;
+    cookieCount.textContent = currentCount;
+    localStorage.setItem('cps', cps)
+}, 1000)
+
 //fetching shop data
 
-async function fetchShopData() {
+/*async function fetchShopData() {
     const response = await fetch(`https://cookie-upgrade-api.vercel.app/api/upgrades`)
     const data = await response.json()
+    console.log(data)
     generateAutoClicker(data)
 }
 
-function generateAutoClicker(data) {
-    autoClicker.innerHTML = ''
-    console.log(data)
-    autoClicker.innerText = data
+function generateAutoClicker(dataToRender) {
+    console.log(dataToRender)
+    for (let i = 0; i < dataToRender.length; i++) {
+        const containerElem = document.createElement('div');
+        //const contentElem = create element for content in div
+        
+        //e.g. titleElem.innerText = dataToRender[i]title
+        //give me title from current i value in array
+        //titleElem.setAttribute('class', 'shopItem')
+        //containerElem.appendchild.titleElem
+        //document.body.appendchild.containerElem
+        
+        //we attach elements to the div, then the div to the page 
+        //or shop div.
+    }
 }
+
+/*function generateAutoClicker(data_to_render) {
+    autoClicker.innerHTML = ''
+    console.log(data_to_render)
+    autoClicker.innerText = data
+}*/
 
 //you cannot afford this notification
 
@@ -54,3 +82,19 @@ function generateAutoClicker(data) {
 
 //make sure things that need to be global variables
 //are in teh scope
+
+//for each element in the api array 
+
+
+//liams example of event listener for shop upgrades
+//if (item.cost >upgrade) {
+   // alert("You need more cookies");
+    //return;
+
+/*if (cookies < item.cost) {
+    alert (cant buy)
+} else {cps += item.cps
+    cookies -= item.cost
+}
+    update content 
+*/ 
